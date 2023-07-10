f9e8e3c5e3d47468a0c453f88c020f1a34b82113
muteshansharma121 <muteshansharma121@email.com>
2023-07-10 16:37:34 +0530

    first commit

---
+++ lib/game_screen.dart
@@ -0,0 +1,234 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:tictactoe_game/home_screen.dart';
+
+class GameScreen extends StatefulWidget {
+  String player1;
+  String player2;
+
+  GameScreen({required this.player1, required this.player2});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  late List<List<String>> _board;
+  late String _currentPlayer;
+  late String _winner;
+  late bool _gameOver;
+
+  @override
+  void initState() {
+    super.initState();
+    _board = List.generate(3, (_) => List.generate(3, (_) => ""));
+    _currentPlayer = "X";
+    _winner = "";
+    _gameOver = false;
+  }
+
+  //Reset Game
+  void _resetGame() {
+    setState(() {
+      _board = List.generate(3, (_) => List.generate(3, (_) => ""));
+      _currentPlayer = "X";
+      _winner = "";
+      _gameOver = false;
+    });
+  }
+
+  void _makeMove(int row, int col) {
+    if (_board[row][col] != "" || _gameOver) {
+      return;
+    }
+
+    setState(() {
+      _board[row][col] = _currentPlayer;
+
+      // check for a winner
+      if (_board[row][0] == _currentPlayer &&
+          _board[row][1] == _currentPlayer &&
+          _board[row][2] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][col] == _currentPlayer &&
+          _board[1][col] == _currentPlayer &&
+          _board[2][col] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][0] == _currentPlayer &&
+          _board[1][1] == _currentPlayer &&
+          _board[2][2] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][2] == _currentPlayer &&
+          _board[1][1] == _currentPlayer &&
+          _board[2][0] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      }
+
+      //switch players
+      _currentPlayer = _currentPlayer == "X" ? "O" : "X";
+
+      //check for a tie
+      if (!_board.any((row) => row.any((cell) => cell == ""))) {
+        _gameOver = true;
+        _winner = "It's a Tie";
+      }
+
+      if (_winner != "") {
+        AwesomeDialog(
+            context: context,
+            dialogType: DialogType.success,
+            animType: AnimType.rightSlide,
+            btnOkText: "Play Again",
+            title: _winner == "X"
+                ? widget.player1 + " Won!"
+                : _winner == "O"
+                    ? widget.player2 + " Won!"
+                    : "It's a Tie",
+            btnOkOnPress: () {
+              _resetGame();
+            })
+          ..show();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF323D5B),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 70),
+            SizedBox(
+              height: 120,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Turn: ",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        _currentPlayer == "X"
+                            ? widget.player1 + " ($_currentPlayer)"
+                            : widget.player2 + " ($_currentPlayer)",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: _currentPlayer == "X"
+                              ? Color(0xFFE25041)
+                              : Color(0xFF1CBD9E),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF5F6B84),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              margin: EdgeInsets.all(5),
+              child: GridView.builder(
+                itemCount: 9,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                ),
+                itemBuilder: (context, index) {
+                  int row = index ~/ 3;
+                  int col = index % 3;
+                  return GestureDetector(
+                    onTap: () => _makeMove(row, col),
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF0E1E3A),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                          child: Text(
+                        _board[row][col],
+                        style: TextStyle(
+                          fontSize: 120,
+                          fontWeight: FontWeight.bold,
+                          color: _board[row][col] == "X"
+                              ? Color(0xFFE25041)
+                              : Color(0xFF1CBD9E),
+                        ),
+                      )),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 18),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: _resetGame,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+                    child: Text(
+                      "Reset Game",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeScreen(),
+                        ));
+                    widget.player1 = "";
+                    widget.player2 = "";
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+                    child: Text(
+                      "Restart Game",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_game/game_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController player1Controller = TextEditingController();
+  final TextEditingController player2Controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF323D5B),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Enter Players Name",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: TextFormField(
+                controller: player1Controller,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  hintText: "Player 1 Name",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter player 1 name";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: TextFormField(
+                controller: player2Controller,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  hintText: "Player 2 Name",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter player 2 name";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                if (_formKey.currentState!.validate()) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => GameScreen(
+                          player1: player1Controller.text,
+                          player2: player2Controller.text,
+                        ),
+                      ));
+                }
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 17, horizontal: 20),
+                child: Text(
+                  "Start Game",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
